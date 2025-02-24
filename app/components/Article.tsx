7b928edc89c437208566ce33c43318d1d2527c16
styxlab <styxlab@localhost.com>
2025-02-24 17:15:09 +0100

    try more

---
+++ app/components/Article.tsx
@@ -0,0 +1,12 @@
+export default function Article() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-col items-center gap-16">
+            <header className="flex flex-col items-center gap-9">
+            <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+                Read this article.
+            </h1>
+            </header>
+        </div>
+        </div>
+    );}
