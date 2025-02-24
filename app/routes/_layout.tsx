c3601390ba54b06160fab6e2f32e256bd6bf4f82
styxlab <styxlab@localhost.com>
2025-02-24 17:46:31 +0100

    add layout and outlet with context

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -3,12 +3,12 @@ import { Outlet, useLoaderData } from '@remix-run/react'
 export const config = { runtime: 'edge' }
 
 export const loader = async () => {
-  return { partner: { name: 'hello world'} }
+  return null
 }
 
 export default function Layout() {
-  const { partner } = useLoaderData<typeof loader>()
-  console.log('partner', partner)
+  //const { partner } = useLoaderData<typeof loader>()
+  //console.log('partner', partner)
 
   return (
       <Outlet />
